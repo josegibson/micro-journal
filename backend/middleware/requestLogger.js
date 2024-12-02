00581bbf8ee46879704d231c4b141598ee98770f
josegibson <josegibson7@gmail.com>
2024-12-02 23:19:33 +0530

    Clean slate of rebuild

--- backend/middleware/requestLogger.js
+++
@@ -1,7 +0,0 @@
-const requestLogger = (req, res, next) => {
-  const userId = req.headers['user-id'] || 'no-user';
-  console.log(`[${req.method}] ${req.path} - User: ${userId}`);
-  next();
-};
-
-module.exports = requestLogger; 
--- micro-journal/src/utils/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export const handleError = (error, fallbackMessage) => {
-  console.error(error);
-  return fallbackMessage || 'An unexpected error occurred.';
-}; 
